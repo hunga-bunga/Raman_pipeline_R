2187cc873f7e7ccf4ed03d74c0219fae776427f1
hunga-bunga <rafael8moreira@gmail.com>
2026-05-31 14:57:39 +0100

    Update chondro.R

--- chondro.R
+++ chondro.R
@@ -203,9 +203,7 @@ airPLS_fast_dataset <- function(dataset, lambda = 1e5, max_iter = 50) {
 
 # --- 2. FUNÇÃO DRPLS (Sem funções auxiliares) ---
 DRPLS <- function(wavenumber, intensity, smoothing) {
-  # O drPLS mimetiza o comportamento aplicando uma penalização adaptativa.
-  # Para manter o código limpo e direto em uma única função, ajustamos 
-  # o peso do lambda para responder ao critério dinâmico do ruído (eta = 1.0).
+
   
   N <- length(intensity)
   
@@ -307,7 +305,7 @@ raman_norm <- function(dataset, method = "min-max", target_peak = 520, search_wi
     stop("Método inválido. Escolha entre 'min-max', 'L2' ou 'silicon'.")
   }
   
-  # Atualizar o dataset com os novos dados e retornar
+  # Atualiza o dataset com os novos dados e retornar
   dataset$data <- norm_data
   return(dataset)
 }
@@ -320,19 +318,19 @@ raman_pca_plot <- function(dataset) {
   # scale. = TRUE é importante se os dados não estiverem perfeitamente normalizados
   pca_res <- prcomp(dataset$data, center = TRUE, scale. = FALSE)
   
-  # 2. Calcular a variância explicada pelas duas primeiras componentes (PC1 e PC2)
+  # 2. Calcula a variância explicada pelas duas primeiras componentes (PC1 e PC2)
   var_explicada <- (pca_res$sdev^2) / sum(pca_res$sdev^2) * 100
   lbl_pc1 <- paste0("PC1 (", round(var_explicada[1], 1), "%)")
   lbl_pc2 <- paste0("PC2 (", round(var_explicada[2], 1), "%)")
   
-  # 3. Criar um data frame com as coordenadas dos pontos e as etiquetas originais
+  # 3. Cria um data frame com as coordenadas dos pontos e as etiquetas originais
   df_pca <- data.frame(
     PC1 = pca_res$x[, 1],
     PC2 = pca_res$x[, 2],
     Classe = as.factor(dataset$y_train)
   )
   
-  # 4. Desenhar o gráfico de dispersão (Scatter Plot) nativo do R
+  # 4. Desenhaa o gráfico de dispersão (Scatter Plot) nativo do R
   # Definir cores bonitas para as tuas 5 classes
   cores_classes <- c("#e63946", "#457b9d", "#1d3557", "#2a9d8f", "#f4a261")
   
